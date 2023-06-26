cc4f612436009816ee6dc265d6f5b341f0af7180
seongs1024 <seongs1024@gmail.com>
2023-06-26 14:18:17 +0000

    expect automation

--- srcs/requirements/tools/gen_cert.sh
+++ srcs/requirements/tools/gen_cert.sh
@@ -1,24 +1,19 @@
+#!/bin/sh
+
+set -e
+
+echo "==Configure tls certifications for the domain=="
 read -p "DOMAIN: " DOMAIN
 
-echo "==GENERATE $DOMAIN.key=="
-openssl genrsa -des3 -out $DOMAIN.key 2048
-if [ $? -ne 0 ]; then
-    echo "\-> FAILED!"
-    exit 1
-fi
-echo "==GENERATE $DOMAIN.csr=="
-openssl req -key $DOMAIN.key -new -out $DOMAIN.csr
-if [ $? -ne 0 ]; then
-    echo "\-> FAILED!"
-    exit 1
-fi
-echo "==GENERATE $DOMAIN.crt=="
-openssl x509 -signkey $DOMAIN.key -in $DOMAIN.csr -req -days 365 -out $DOMAIN.crt
-if [ $? -ne 0 ]; then
-    echo "\-> FAILED!"
-    exit 1
-fi
-echo "==GENERATE $DOMAIN.pass=="
 touch $DOMAIN.pass
 read -sp "Password: " PASSWORD
 echo $PASSWORD >> $DOMAIN.pass
+
+printf "\n==GENERATE $DOMAIN.key==\n"
+expect gen_ssl_key.exp ${DOMAIN} ${PASSWORD}
+
+printf "\n==GENERATE $DOMAIN.csr==\n"
+expect gen_ssl_csr.exp ${DOMAIN} ${PASSWORD}
+
+printf "\n==GENERATE $DOMAIN.crt==\n"
+expect gen_ssl_crt.exp ${DOMAIN} ${PASSWORD}
