90e81a91103ebd8c0cba760926ae87843349b233
Seongsu Park <seongspa@c5r2s1.42seoul.kr>
2023-09-14 14:35:39 +0900

    deployed simple nginx server

--- srcs/requirements/tools/make_cert.sh
+++ srcs/requirements/tools/make_cert.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
-DOMAIN=seongspa.42.fr
+
+read -p "DOMAIN=" DOMAIN
+read -p "PASS=" PASS
+echo ${PASS} > ${DOMAIN}.pass
 openssl genrsa -des3 -out ${DOMAIN}.key 2048
 openssl req -key ${DOMAIN}.key -new -out ${DOMAIN}.csr
 openssl x509 -signkey ${DOMAIN}.key -in ${DOMAIN}.csr -req -days 365 -out ${DOMAIN}.crt
