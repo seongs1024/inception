67f76fd3b918a966980b174ab2e272d17d9a8b04
Seongsu Park <seongspa@c5r2s1.42seoul.kr>
2023-09-15 23:13:25 +0900

    wp cannot connect to db

--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -1,18 +1,15 @@
-#!/usr/bin/sh
+#!/bin/sh
+set -e
+cd /var/www
 if [ ! -f "/var/www/wp-config.php" ]; then
-cat << EOF > /var/www/wp-config.php
-<?php
-define( 'DB_NAME', '${DB_NAME}' );
-define( 'DB_USER', '${DB_USER}' );
-define( 'DB_PASSWORD', '${DB_USER_PASS}' );
-define( 'DB_HOST', 'localhost' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
-define('FS_METHOD','direct');
-\$table_prefix = 'wp_';
-define( 'WP_DEBUG', false );
-if ( ! defined( 'ABSPATH' ) ) {
-define( 'ABSPATH', __DIR__ . '/' );}
-require_once ABSPATH . 'wp-settings.php';
-EOF
+	#sed -i "s/username_here/${DB_USER}/g" wp-config-sample.php
+	#sed -i "s/password_here/${DB_USER_PASS}/g" wp-config-sample.php
+	#sed -i "s/localhost/mariadb/g" wp-config-sample.php
+	#sed -i "s/database_name_here/${DB_NAME}/g" wp-config-sample.php
+	#mv wp-config-sample.php wp-config.php
+	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_USER_PASS} --dbhost=mariadb --allow-root
+	wp core install --url=${DOMAIN} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --allow-root
+	wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASS} --allow-root
 fi
+#exec "$@"
+exec /usr/sbin/php-fpm81 -F
